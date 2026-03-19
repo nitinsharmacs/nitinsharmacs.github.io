d9ab4c4aea14f6862e14df87b30370de64d2bd77
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2026-03-19 16:31:17 +0530

    Add giscus comment config

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,23 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        // from data-repo
+        repo: "nitinsharmacs/nitinsharmacs.github.io",
+        // from data-repo-id
+        repoId: "R_kgDOOnzFFQ",
+        // from data-category
+        category: "Announcements",
+        // from data-category-id
+        categoryId: "DIC_kwDOOnzFFc4C4xGj",
+        // from data-lang
+        lang: "en",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       "Made with ❤️ by Nitin": "https://github.com/nitinsharmacs",
